2c1d3b5556837e91ebc9a0c575c7ce8ef5217d55
Study Material <40472233+jarihant130@users.noreply.github.com>
2023-02-26 13:56:34 +0530

    Updating PDF to Word function

--- app.py
+++ app.py
@@ -204,19 +204,21 @@ def word2pdf():
     except Exception as e:
         st.error(f"Error: {e}")
         
+# Function to convert pdf to docx        
 def pdf2word():
     st.title("Convert PDF to WORD")
     st.subheader("Welcome to PDF TO Word Document Converter!")
     st.write("This is a simple application that convert PDF to word document.")
+
     try:
         # File uploader
         uploaded_file = st.file_uploader("Upload a PDF File", type=["pdf"])
 
         if uploaded_file is not None:
             # Ask user for output file name
-            output_file_name = st.text_input("Output file name", "output.doc")
+            output_file_name = st.text_input("Output file name", "output.docx")
 
-            if st.button("Convert to word"):
+            if st.button("Convert to Word"):
                 if uploaded_file is None:
                     st.warning("Please upload a PDF file first!")
                 else:
@@ -234,16 +236,22 @@ def pdf2word():
 
                         # Add the text to the Word document
                         doc.add_paragraph(text)
-                    # Show success message
-                    st.success("Conversion successful!")
-                    
+
                     # Save the Word document
                     doc.save(output_file_name)
 
+                    # Show success message
+                    st.success("Conversion successful!")
 
-                    # Show download success message
-                    st.success("Your Word document file has been downloaded successfully!")
-            
+                    # Create a download button for the converted file
+                    with open(output_file_name, 'rb') as f:
+                        bytes_data = f.read()
+                    st.download_button(
+                        label="Download Converted File",
+                        data=bytes_data,
+                        file_name=output_file_name,
+                        mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+                    )
     except Exception as e:
         st.error(f"Error: {e}")
 
