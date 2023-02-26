a286c8fcadfa66e42bc735c7f453f61deaca13d5
Study Material <40472233+jarihant130@users.noreply.github.com>
2023-02-26 19:50:57 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,5 +1,5 @@
 import streamlit as st
 def home():
-#     st.set_page_config(page_title="Convert Anything!!", page_icon=":pencil2:", layout="wide")
+    st.set_page_config(page_title="Convert Anything!!", page_icon=":pencil2:", layout="wide")
     st.subheader("Convert Anything From Here!!")
     st.write("Please select available options from the menu to get started.")
