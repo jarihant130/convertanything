303abfdec17710855410a4f5039c5f387ca30392
Study Material <40472233+jarihant130@users.noreply.github.com>
2023-02-26 23:15:46 +0530

    Add files via upload

--- home.py
+++ home.py
@@ -1,5 +1,14 @@
 import streamlit as st
+from PIL import Image
 def home():
-#     st.set_page_config(page_title="Convert Anything!!", page_icon=":pencil2:", layout="wide")
-    st.subheader("Convert Anything From Here!!")
-    st.write("Please select available options from the menu to get started.")
+    try:
+        st.subheader("Convert Anything From Here!!")
+        st.write("Please select available options from the menu to get started.")
+
+        # Load image from file
+        image = Image.open("convert_anything.jpg")
+
+        # Display image on Streamlit app
+        st.image(image, caption="Convert Anything")
+    except Exception as e:
+        st.error(f"Error: {e}")
