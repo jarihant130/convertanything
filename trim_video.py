4a23bd9dd2d11e95ae39cb5fd152c05265cd88a9
Study Material <40472233+jarihant130@users.noreply.github.com>
2023-03-01 22:33:33 +0530

    Trim Video code has been updated

--- trim_video.py
+++ trim_video.py
@@ -1,51 +1,56 @@
 import streamlit as st
-import moviepy.editor as mp
 from moviepy.video.io.VideoFileClip import VideoFileClip
 import tempfile
-from io import BytesIO
 
 def trim_video():
-    st.subheader("Welcome to Video Trimmer!")
-    st.write("This is a simple application that trim the video.")
+    
+    st.title("Video Trimmer App :scissors:")
 
-    # Upload the video
-    uploaded_file = st.file_uploader("Choose a video file", type=["mp4", "avi", "mov"])
-    # Display the uploaded video
-    st.video(uploaded_file)
+    # Upload a video file
+    video_file = st.file_uploader("Upload a video", type=["mp4"])
         
-    if uploaded_file is not None and uploaded_file.name.endswith('.mp4'):
-        # Save the uploaded file to a temporary file
-        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-            temp_file.write(uploaded_file.read())
-            temp_file.flush()
+    if video_file:
+        # Load the video file
+        video = VideoFileClip(video_file.name)
+        # Get the start and end times for the trim
+        start_time = st.slider("Start time (in seconds)", 0, int(video.duration))
+        end_time = st.slider("End time (in seconds)", start_time, int(video.duration))
+        try:
+            # Trim the video from start_time to end_time
+            trimmed_video = video.subclip(start_time, end_time)
+            
+            # Output file name
+            output_filename = st.text_input("Enter the desired output filename", value="trimmed.mp4")
+            
+            # Download the trimmed video as a file
+            with st.spinner("Downloading video..."):
+                # Create a temporary file to save the trimmed video
+                with tempfile.NamedTemporaryFile(suffix=".mp4", delete=False) as f:
+                    file_path = f.name
+                    trimmed_video.write_videofile(file_path)
 
-        # Read the video file
-        video = VideoFileClip(temp_file.name)
+                # Display the trimmed video
+                with open(file_path, "rb") as f:
+                    video_bytes = f.read()
+                    st.video(video_bytes)
 
-        # Get the length of the video in seconds
-        video_length = int(video.duration)
-
-        # Get the start and end times from the user
-        start_time = st.slider("Start time (in seconds)", 0, video_length, 0)
-        end_time = st.slider("End time (in seconds)", start_time, video_length, video_length)
-
-        # Trim the video
-        trimmed_video = video.subclip(start_time, end_time)
-
-        # Convert the trimmed video to a VideoFileClip object
-        trimmed_video = trimmed_video.to_videofile("trimmed_video.mp4", fps=video.fps or 30)
-        st.write(trimmed_video)
-
-        # Display the trimmed video
-        st.video(trimmed_video)
-
-        # Save the trimmed video to a file
-        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-            trimmed_video.write_videofile("Trimmed.mp4")
-
-            # Download the trimmed video
-            with open(temp_file.name, 'rb') as f:
-                video_bytes = BytesIO(f.read()).getvalue()
-            st.download_button("Download trimmed video", data=video_bytes, file_name="trimmed_video.mp4")
+            # Add a button to download the trimmed video
+            st.download_button(
+                label="Download trimmed video",
+                data=video_bytes,
+                file_name=output_filename,
+                mime="video/mp4"
+            )
+        except Exception as e:
+            st.error(f"Error: {e}")
     else:
-        st.warning("Please upload a video file")
+        st.warning("Please upload the video file to trim!!")
+            
+    
+
+
+
+
+
+
+
