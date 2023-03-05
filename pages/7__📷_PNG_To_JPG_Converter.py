6ce3bcbb6368a1d7ea8899f565b4e00dbe1d5ecf
Study Material <40472233+jarihant130@users.noreply.github.com>
2023-03-05 12:15:29 +0530

    Fixing the output file format

--- pages/7__📷_PNG_To_JPG_Converter.py
+++ pages/7__📷_PNG_To_JPG_Converter.py
@@ -4,8 +4,9 @@ from io import BytesIO
 import base64
 
 def pngtojpg():
-    st.title("PNG to JPG Converter")
-    st.write("This application helps you to convert your image from PNG to JPG format.")
+    st.set_page_config(page_title="PNG to JPG Converter!!", page_icon=":camera:", layout="wide")
+    st.title("PNG to JPG Converter :camera:")
+    st.write("This Streamlit application allows you to convert your PNG image to JPG format. Upload your image and enter the desired output file name. The application will display the image and provide a download button to save the converted image as a JPG file. The application uses the PIL library to convert the uploaded image to RGB mode and the io and base64 libraries to handle the file conversion and download.")
     # Upload the image
     uploaded_file = st.file_uploader("Choose an image", type=["png"])
     if uploaded_file is not None:
@@ -17,16 +18,16 @@ def pngtojpg():
             image = image.convert('RGB')
             
         st.image(image)
-#         image.save(output_file)
         
         # Add a "Download" button to allow the user to download the cropped image
         with BytesIO() as output:
-            image.save(output_file)
+            image.save(output, format='JPEG')
+            output.seek(0)
             b64 = base64.b64encode(output.getvalue()).decode()
-            href = f'<a href="data:application/octet-stream;base64,{b64}" download={output_file}>Download Image</a>'
+            href = f'<a href="data:application/octet-stream;base64,{b64}" download="{output_file}">Download Image</a>'
             st.markdown(href, unsafe_allow_html=True)
     else:
         st.warning("Please upload an image file first!")
 
 if __name__ == '__main__':
-    pngtojpg()               
+    pngtojpg() 
